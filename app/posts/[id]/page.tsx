5ca6dfe6e0717165c26bb67c198e1856a86894f7
Nikolas Burk <nikolas.burk@gmail.com>
2025-02-12 14:44:37 +0100

    re-add enforce dynamic

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic"; // This disables SSG and ISR
+
 import prisma from "@/lib/prisma";
 import { notFound, redirect } from "next/navigation";
 
--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic"; // This disables SSG and ISR
+
 import prisma from "@/lib/prisma";
 import { redirect } from "next/navigation";
 import Form from "next/form";
