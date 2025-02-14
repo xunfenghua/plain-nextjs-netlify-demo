6097b7172e323b88d3033af9084a16c7e4da0090
Nikolas Burk <nikolas.burk@gmail.com>
2025-02-14 17:10:02 +0100

    polish setup page

--- app/setup/page.tsx
+++ app/setup/page.tsx
@@ -14,7 +14,7 @@ export default function SetupPage() {
           {/* Step 1 */}
           <div className="mt-6">
             <h2 className="text-2xl font-semibold text-gray-900">1. Create a Prisma Postgres instance</h2>
-            <p className="text-gray-700 mt-2">Create a new Prisma Postgres database instance using:</p>
+            <p className="text-gray-700 mt-2">Create a new Prisma Postgres database instance:</p>
             <ol className="list-decimal pl-6 mt-2 space-y-1 text-gray-700">
               <li>
                 Go to{" "}
@@ -27,8 +27,7 @@ export default function SetupPage() {
                 Click <strong>New project</strong> to create a new project.
               </li>
               <li>
-                Enter a name for your project in the <strong>Name</strong> field. (Remember the value you provide since
-                you will need it in <strong>Step 3</strong>).
+                Enter a name for your project in the <strong>Name</strong> field.
               </li>
               <li>
                 Inside the <strong>Prisma Postgres</strong> section, click <strong>Get started</strong>.
@@ -44,13 +43,17 @@ export default function SetupPage() {
 
           {/* Step 2 */}
           <div className="mt-6">
-            <h2 className="text-2xl font-semibold text-gray-900">2. Add the Prisma Integration Token to Netlify</h2>
+            <h2 className="text-2xl font-semibold text-gray-900">2. Add the Prisma integration token to Netlify</h2>
             <p className="text-gray-700 mt-2">
               When you created the site with this template, you already installed the{" "}
-              <a href="" target="_blank">
+              <a
+                href="https://app.netlify.com/extensions/prisma-postgres"
+                className="text-blue-600 hover:underline"
+                target="_blank"
+              >
                 Prisma Postgres extension
-              </a>
-              . Next, you need to copy Prisma&apos;s Integration Token into the extension:
+              </a>{" "}
+              in your Netlify account. Next, you need to copy Prisma&apos;s integration token into the extension in your Neltify Dashboard:
             </p>
             <ol className="list-decimal pl-6 mt-2 space-y-1 text-gray-700">
               <li>
@@ -58,7 +61,7 @@ export default function SetupPage() {
                 <a href="https://console.prisma.io" className="text-blue-600 hover:underline" target="_blank">
                   Prisma Console
                 </a>
-                , click the <strong>Integrations</strong> tab in the left-hand sidenav.
+                , navigate to the root of your Workspace and click the <strong>Integrations</strong> tab in the left-hand sidenav.
               </li>
               <li>
                 In the <strong>Configure Team settings</strong> section, click the{" "}
@@ -109,48 +112,6 @@ export default function SetupPage() {
 
           {/* Step 4 */}
           <div className="mt-6">
-            <h2 className="text-2xl font-semibold text-gray-900">4. Migrate and seed the database</h2>
-            <p className="text-gray-700 mt-2">
-              In this section, you need to migrate the database by running a Prisma CLI command. This is best done from
-              your local machine.
-            </p>
-            <p className="text-gray-700 mt-2">
-              First, clone the Git repo that Netlify created in your account, navigate into it and install dependencies
-              (do not forget to replace the <code>USERNAME</code> placeholder with your actual username, and potentially
-              adjust the name of the repo if you gave it a different name):
-            </p>
-            <pre className="mt-4 p-4 bg-gray-900 text-white text-sm rounded-lg overflow-auto">
-              git clone git@github.com:USERNAME/nextjs-prisma-postgres-starter.git
-              <br />
-              cd nextjs-prisma-postgres-starter
-              <br />
-              npm install
-            </pre>
-            <p className="text-gray-700 mt-2">
-              Create a <code>.env</code> file and add the <code>DATABASE_URL</code> environment variable to it (you can
-              find the value for that in the project you created in <strong>Step 1</strong> in the{" "}
-              <a href="https://console.prisma.io" className="text-blue-600 hover:underline" target="_blank">
-                Prisma Console
-              </a>
-              ):
-            </p>
-            <pre className="mt-4 p-4 bg-gray-900 text-white text-sm rounded-lg overflow-auto">
-              # .env <br></br>
-              DATABASE_URL=&quot;prisma+postgres://accelerate.prisma-data.net/?api_key=ey...&quot;
-            </pre>
-            <p className="text-gray-700 mt-2">
-              You can now migrate your database and apply the schema defined in <code>prisma/schema.prisma</code> with
-              this command:
-            </p>
-            <pre className="mt-4 p-4 bg-gray-900 text-white text-sm rounded-lg overflow-auto">
-              npx prisma migrate dev --name init
-            </pre>
-            <p className="text-gray-700 mt-2">Finally, run this command to seed the database with some sample data:</p>
-            <pre className="mt-4 p-4 bg-gray-900 text-white text-sm rounded-lg overflow-auto">npx prisma db seed</pre>
-          </div>
-
-          {/* Step 5 */}
-          <div className="mt-6">
             <h2 className="text-2xl font-semibold text-gray-900">5. Re-deploy the site in Netlify</h2>
             <p className="text-gray-700 mt-2">
               Your site is now fully configured to load data from the Prisma Postgres database you just created, the
@@ -172,7 +133,7 @@ export default function SetupPage() {
             </ol>
             <p className="text-gray-700 mt-2">
               {" "}
-              Once the deployment is ready, you can click on <strong>Open production deploy</strong> and use the app 🎉
+              Once the deployment is ready, you can click on <strong>Open production deploy</strong> and use the app by creating users and posts via the UI 🎉
             </p>
           </div>
         </div>
