8aaa3ca07f9dd1b46e2a5243596a750b8eb86fa4
xunfenghua <162309486+xunfenghua@users.noreply.github.com>
2025-03-06 20:16:21 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/admin/(.*)",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "text/html",
+          },
+        ],
+      },
+    ];
+  },
 };
-
-export default nextConfig;
